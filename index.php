acae623937cef808e48f9ef1faf5b3b23564c368
Septian <septgsk@gmail.com>
2022-08-05 01:45:14 +0700

    test

--- index.php
+++ index.php
@@ -3,13 +3,5 @@ $f3 = require('/fff/base.php');
 $f3->route('GET /', function () {
     echo 'Hello Worlds';
 });
-$f3->route(
-    'GET /about',
-    function () {
-        echo 'About page';
-    }
-);
-
-
 
 $f3->run();
